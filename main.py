e07d2d3679aefbc1845dabecad29794ba2a8fc5f
Zhengjie Zhou (Jay) <zhengjiezhou@dal.ca>
2023-02-21 17:52:44 -0500

    Endpoint to update user

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -13,7 +13,7 @@ class User(db.Model):
     first_name = db.Column(db.String(50), nullable=False)
     last_name = db.Column(db.String(50))
     company_id = db.Column(db.Integer, db.ForeignKey('company.id'))
-    email = db.Column(db.String(255), nullable=False)
+    email = db.Column(db.String(255), unique=True, nullable=False)
     phone = db.Column(db.String(50))
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
     updated_at = db.Column(
@@ -71,7 +71,7 @@ class Company(db.Model):
         backref=db.backref('company', lazy=False))
 
     def __repr__(self):
-        return '<Company %r>' % self.id
+        return '<Company %r>' % self.id 
 
 @app.route("/")
 def hello_world():
@@ -91,3 +91,46 @@ def get_user(user_id):
 def get_user_by_email(email):
     user = User.query.filter_by(email=email).first_or_404()
     return jsonify(user.to_dict())
+
+@app.route("/users/<int:user_id>", methods=['PUT'])
+def update_user(user_id):
+    user = User.query.get_or_404(user_id)
+    if 'name' in request.json:
+        user.first_name = request.json['name'].split()[0]
+        user.last_name = request.json['name'].split()[1]
+    if 'email' in request.json:
+        user.email = request.json['email']
+    if 'phone' in request.json:
+        user.phone = request.json['phone']
+
+    if 'company' in request.json:
+        old_company = user.company
+        new_company = Company.query \
+            .filter_by(name=request.json['company']).first()
+        if new_company is None:
+            new_company = Company(name=request.json['company'])
+            db.session.add(new_company)
+        user.company = new_company
+
+    if 'skills' in request.json:
+        for old_user_skill in UserSkill.query.filter_by(user_id=user.id).all():
+            db.session.delete(old_user_skill)
+
+        for user_skill_data in request.json['skills']:
+            skill_name = user_skill_data['skill']
+            skill_rating = user_skill_data['rating']
+            skill = Skill.query.filter_by(name=skill_name).first()
+
+            if skill is None:
+                skill = Skill(name=skill_name)
+                db.session.add(skill)
+
+            user_skill = UserSkill(
+                user=user, skill=skill, rating=skill_rating)
+    db.session.commit()
+    return jsonify(user.to_dict())
+
+@app.route("/users/<string:email>", methods=['PUT'])
+def update_user_by_email(email):
+    user = User.query.filter_by(email=email).first_or_404()
+    return update_user(user.id)
